e609e3e1e48f150186023c2fd4ab329c866fcd4b
Tim Clemans <tim@insideyourgovernment.com>
2016-01-23 12:48:17 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
 import json
+import uuid
 from flask import request, jsonify
 @app.route("/add_alert/")
 def add_alert():
+    random_id = uuid.uuid4()
     with open('data.json', 'r') as f:
         data = json.loads(f.read())
         data.append({"id": random_id, "email": request.args.get('email'), "url": request.args.get('url'), "confirmed": False})
@@ -19,8 +21,7 @@ def add_alert():
     message = sendgrid.Mail()
     message.add_to(request.args.get('email'))
     message.set_subject('Example')
-    message.set_html('Body')
-    message.set_text('Body')
+    message.set_html('<a href="'+configuration['domain']+'/confirm_alert/?id='+random_id+'">Click here to confirm</a>')
     message.set_from('Doe John <doe@email.com>')
     status, msg = sg.send(message)
     return jsonify(success=True)        
