eef13173c6511902cb46c6e0c388cef2081777ec
Tim Clemans <tim@insideyourgovernment.com>
2016-01-24 11:36:49 -0800

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -10,7 +10,9 @@ while True:
             result = requests.get('%s&$limit=1&$select=:created_at&$order=:created_at DESC&$$app_token=%s' % (alert_job[url], configuration['socrata_app_token'])).json()
             alert_job['last_created_at'] = result[':created_at']
         elif alert_job['confirmed']:
-            the_data = requests.get('%s&$app_token=%s' % (alert_job[url], configuration['socrata_app_token'])).json()
+            the_data = requests.get('%s&$where=:created_at%%20>%%20"%s"&$app_token=%s' % (alert_job[url], alert_job['last_created_at'], configuration['socrata_app_token'])).json()
+            result = requests.get('%s&$limit=1&$select=:created_at&$order=:created_at DESC&$$app_token=%s' % (alert_job[url], configuration['socrata_app_token'])).json()
+            alert_job['last_created_at'] = result[':created_at']
             if not the_data:
                 continue
             html_of_data = '<table>'
